cc4cfb055295a4134c712cce02ff33172b161724
Van Vu <vanvn.vu@gmail.com>
2023-08-04 11:35:25 +0700

    Added access token check

--- src/services/workspace.service.js
+++ src/services/workspace.service.js
@@ -1,10 +1,19 @@
 import axios from "axios";
+import cookieParse from "./cookieparse.service";
+
+const authKey = cookieParse()._auth;
 
 export default class WorkspaceService {
   static async createWorkspace(data) {
     return await axios.post(
       `${import.meta.env.VITE_ROOT_DOMAIN}/workspaces`,
-      data
+      data,
+      {
+        headers: {
+          "content-type": "text/json",
+          authorization: authKey,
+        },
+      }
     );
   }
 }
