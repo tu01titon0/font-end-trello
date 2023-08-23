e43c0d389e06ab5f3d17ee1e5c83626d96630192
Van Vu <vanvn.vu@gmail.com>
2023-08-23 13:26:20 +0700

    fixes cors

--- src/miscs/socket.js
+++ src/miscs/socket.js
@@ -1,3 +1,5 @@
 import io from "socket.io-client";
 
-export const socket = io(import.meta.env.VITE_SOCKET_DOMAIN);
+export const socket = io(import.meta.env.VITE_SOCKET_DOMAIN, {
+  transports: ["websocket", "polling", "flashsocket"],
+});
